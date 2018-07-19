3a4c7e356a62822447afad98f6a02a902e6043ee
Martin Haagmans <mahaagmans@gmail.com>
2018-07-19 12:24:06 +0200

    Create map_amplicons.sh

---
+++ map_amplicons.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# $REF should be set
+
+for i in $(ls *.gz | cut -d "_" -f 1 | sort | uniq) ; do
+  R1=$(ls $i\_*R1*.gz) ;  
+  R2=$(ls $i\_*R2*.gz) ;  
+  echo $R1 $R2 ; 
+  bwa mem  -R "@RG\\tID:$i\\tLB:$i\\tPL:ILLUMINA\\tPU:$i\\tSM:$i" -t 1 -M $REF $R1 $R2 |\
+  samtools view -Shu - |\
+  samtools sort -T $i.tmp -O bam - > $i.sorted.bam ; 
+  samtools index $i.sorted.bam ;  
+done ;
