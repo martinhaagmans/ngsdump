b56531663063941a40a1bf3203b7fac7ca6e44e7
Martin Haagmans <mahaagmans@gmail.com>
2018-07-30 12:02:24 +0200

    only remove after succes

--- transfer_to_production.sh
+++ transfer_to_production.sh
@@ -3,8 +3,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 for i in $(ls $DIR/MS*.txt) ; do
-	scp $i ux-p-dnadiag:$DIR ;
-	rm -f $i ;
+	scp $i ux-p-dnadiag:$DIR && rm -f $i ;
 done ;
 
 ssh ux-p-dnadiag "bash $DIR/add_to_db.sh" 
