fdcf1e5faa0f963c61e3975125351eb0c32fb1cc
Martin Haagmans <mahaagmans@gmail.com>
2018-07-30 12:01:50 +0200

    pyCNV => CNV

--- add_to_db.sh
+++ add_to_db.sh
@@ -3,7 +3,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 for i in $(ls $DIR/MS*.txt) ; do
-	pyCNV --create -c BLA -i $i && mv $i $DIR/toegevoegd
+	CNV --create -c BLA -i $i && mv $i $DIR/toegevoegd
 done ;
 
 
