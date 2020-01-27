21f93f7c74dd258b8b759e7d2f08f87d74a28422
Martin Haagmans <mahaagmans@gmail.com>
2020-01-27 10:51:03 +0100

    Create add_patientinfo_to_db.py

---
+++ add_patientinfo_to_db.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+DB = '/data/dnadiag/databases/patientinfo.sqlite'
+
+
+def read_patientinfo(patientinfo):
+    data = list()
+    serie = patientinfo.split('_')[0].replace('MS', '')
+    with open(patientinfo) as f:
+        for line in f:
+            if line not in ['\n', '\r\n']:
+                data.append('{}\t{}'.format(serie, line))
+    return data
+
+
+def add_to_db(data):
+    conn = sqlite3.connect(DB)
+    c = conn.cursor()    
+    for line in data:
+        serie, sample, sex, ff, dob = line.split()
+        try:
+            c.execute('INSERT INTO patientinfo VALUES ("{}", "{}", "{}", "{}", "{}")'.format(serie, sample, sex, ff, dob))
+        except sqlite3.IntegrityError as e:
+            print(e)
+        else:
+            conn.commit()
+    conn.close()
+    return
+
+
+def main(patientinfo):
+    data = read_patientinfo(patientinfo)
+    add_to_db(data)
+
+
+if __name__ == '__main__':
+    import argparse
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", type=str, 
+                        help="File met patient info", required=True)
+    args = parser.parse_args()
+    main(args.input)
